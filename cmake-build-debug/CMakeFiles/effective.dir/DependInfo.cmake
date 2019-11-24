13b21a0f8aac546d687634c8a801e7ade7a1a0e8
Sunriseyms <1244373211@qq.com>
2019-11-24 22:49:23 +0800

    eclipse project reverse to clion(with cmake)

---
+++ cmake-build-debug/CMakeFiles/effective.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yangmaosheng/CLionProjects/Effective_STL/src/EffectiveStl.cpp" "/Users/yangmaosheng/CLionProjects/Effective_STL/cmake-build-debug/CMakeFiles/effective.dir/src/EffectiveStl.cpp.o"
+  "/Users/yangmaosheng/CLionProjects/Effective_STL/src/EffectiveStlRule.cpp" "/Users/yangmaosheng/CLionProjects/Effective_STL/cmake-build-debug/CMakeFiles/effective.dir/src/EffectiveStlRule.cpp.o"
+  "/Users/yangmaosheng/CLionProjects/Effective_STL/src/Knowledge_Surrounding/MethodObjectPointLearn.cpp" "/Users/yangmaosheng/CLionProjects/Effective_STL/cmake-build-debug/CMakeFiles/effective.dir/src/Knowledge_Surrounding/MethodObjectPointLearn.cpp.o"
+  "/Users/yangmaosheng/CLionProjects/Effective_STL/src/StlElementIsPoint.cpp" "/Users/yangmaosheng/CLionProjects/Effective_STL/cmake-build-debug/CMakeFiles/effective.dir/src/StlElementIsPoint.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/Knowledge_Surrounding"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/effective.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/effective.dir/src/EffectiveStl.cpp.o"
+  "CMakeFiles/effective.dir/src/EffectiveStlRule.cpp.o"
+  "CMakeFiles/effective.dir/src/Knowledge_Surrounding/MethodObjectPointLearn.cpp.o"
+  "CMakeFiles/effective.dir/src/StlElementIsPoint.cpp.o"
+  "effective"
+  "effective.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/effective.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
